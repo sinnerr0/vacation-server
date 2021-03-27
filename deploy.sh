d09c528dda2d085b31da7ce690bb351537d521b5
kschoi <sinnerr0gmail.com>
2021-03-27 18:32:58 +0900

    chore: modify

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,14 @@ docker stop vacation-server
 docker stop vacation-cron
 
 docker build -t vacation-server ./server
+
 docker run --name vacation-server --rm -v /home/ubuntu/www:/app/www -dp 3001:3001 vacation-server
 
 docker build -t vacation-cron \
   --build-arg SESSION=$SESSION \
   --build-arg SERVICE_KEY=$SERVICE_KEY \
   ./cron
+
 docker run --name vacation-cron --rm -d -v /home/ubuntu/www:/app/www vacation-cron
 
 echo "finished"
